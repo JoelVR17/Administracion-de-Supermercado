afaf6ab32a86af769e86ef108f328ddaab42e505
Gabo1696 <79115546+Gabo1696@users.noreply.github.com>
2022-07-12 17:28:17 -0600

    indexadmin
    indexadmin junto a su css

---
+++ merca_super/indexadmin.php
@@ -0,0 +1,77 @@
+<!-- HTML5 -->
+<!DOCTYPE html>
+
+<!-- HTML -->
+<html lang="en">
+
+<!-- HEAD -->
+
+<head>
+    <?php require_once "scripts.php"; ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../merca_super/style/indexadmin.css">
+    
+</head>
+ <!--fin.Head-->
+
+<!-- BODY -->
+<body>
+<div class="principal">
+        <div class="container-fluid">
+            <div class="container-titulo">
+                <p class="merca">Merca<br><span class="super">Súper</span></p>
+            </div>
+            <div class="menu">
+                
+                <a class="navbar-brand" href="">
+                    <p class="textotitulo">Inventario</p>
+                </a>
+               <a class="navbar-brand" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="50" fill="currentColor" class="bibi-cart"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                        <path
+                            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                    </svg>
+                </a>
+                <a class="navbar-brand" href="">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="50" fill="currentColor" class="bibi-cart"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                        <path
+                            d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+        <div class="contenido">
+            <div class="imagen">
+                <img src="./img/logo.png">
+            </div>
+            <div class="titulo">
+                <h1>Merca</h1>
+                <h1>Super</h1>
+            </div>
+            <div class="info">
+                <h1>Bienvenido</h1>
+
+                <p>!ve los pedidos!</p>
+                <button> ir</button>
+                <br> <br>
+                <p>!ve el inventario!</p>
+                <button> ir</button>
+            </div>
+
+        </div>
+    </div>
+
+</body> <!--fin.body-->
+
+</html> <!--fin.html-->
