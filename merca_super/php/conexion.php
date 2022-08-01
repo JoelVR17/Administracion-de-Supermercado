2aa871ef6c1e9fb5a6363b21515a2cb812865d3e
Marco Araya Jimenez <Marcojime23@gmail.com>
2022-08-01 16:13:57 -0500

    Commit borrar carpeta php
    Se borrar carpeta innecesaria de php

--- merca_super/php/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    function conexion () {
-        // Aqui se usa MySQL en vez de phpMyAdmin entonces debe ponerse la contraseña de MySQL Workbench
-        return $conexion = mysqli_connect("localhost","root","mantoara2310","proyecto_web");
-    }
-
-
-?>
--- merca_super/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['user']);
-    header("location:../login.php");
-?>
-
